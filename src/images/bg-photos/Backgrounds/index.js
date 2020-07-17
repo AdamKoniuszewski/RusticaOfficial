cb0dcca37d91ffd0953dbdfea2d8db029504fa51
Adam Koniuszewski <arkoniuszewski@gmail.com>
2020-07-17 19:53:57 +0200

    photo size test

--- src/images/bg-photos/Backgrounds/index.js
+++ src/images/bg-photos/Backgrounds/index.js
@@ -1,6 +1,6 @@
 
-export const bg1 = require("./1.jpg");
-export const bg2 = require("./2.jpg");
-export const bg3 = require("./3.jpg");
-export const bg4 = require("./4.jpg");
+export const bg1 = require("./12.jpg");
+export const bg2 = require("./22.jpg");
+export const bg3 = require("./32.jpg");
+export const bg4 = require("./42.jpg");
 
