d682788c69661f40d699995c4745b10e327faecd
Adam Koniuszewski <arkoniuszewski@gmail.com>
2020-07-17 20:23:38 +0200

    erased transition - optimization purposes

--- src/components/LandingSection/LandingSection-comp.js
+++ src/components/LandingSection/LandingSection-comp.js
@@ -2,34 +2,36 @@ import React from "react";
 import "./LandingSection-style.scss";
 import LandingBody from "../LandingBody/LandingBody-comp";
 import Header from "../Header/Header-comp";
+import bgPhoto from "../../images/bg-photos/Backgrounds/1.jpg"
+/*
 import * as imageBundle from "../../images/bg-photos/Backgrounds/index";
-
 const images=[
     imageBundle.bg1,
     imageBundle.bg2,
     imageBundle.bg3,
     imageBundle.bg4,
 ]
-
-console.log(images);
-
 var x = 0;
+*/
+
+
+
 
 class LandingSection extends React.Component {
     constructor(props){
     super(props);
         this.state = {
-            imgPath: images[x],
+            imgPath: bgPhoto,
         }
     }
 
 
     componentDidMount() {
-        this.interval = setInterval(()=>{
+       /* this.interval = setInterval(()=>{
             x>=images.length-1? x=0:x++;            
             this.setState({imgPath: images[x]});
             console.log(x + " and path " +this.state.imgPath)},
-            6000); 
+            6000); */
     }
 
     componentWillUnmount() {
